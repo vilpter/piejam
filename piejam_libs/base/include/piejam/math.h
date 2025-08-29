ddcecffe2d6fb52263c0f8bdbe923eea15a5057a
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2025-08-29 20:14:07 +0200

    base: add almost_equal and exact_equal for float comparisons

--- piejam_libs/base/include/piejam/math.h
+++ piejam_libs/base/include/piejam/math.h
@@ -17,6 +17,35 @@ namespace piejam::math
 {
 
 template <std::floating_point T>
+[[nodiscard]]
+constexpr auto
+almost_equal(T a, T b) noexcept -> bool
+{
+    constexpr T abs_eps =
+            std::numeric_limits<T>::epsilon() * 100; // near-zero tolerance
+    constexpr T rel_eps = static_cast<T>(1e-5);      // relative tolerance
+
+    T const diff = (a > b) ? (a - b) : (b - a);
+    if (diff <= abs_eps)
+    {
+        return true;
+    }
+
+    T const largest = (a > b) ? a : b;
+    return diff <= largest * rel_eps;
+}
+
+template <std::floating_point T>
+constexpr auto
+exact_equal(T a, T b) noexcept -> bool
+{
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wfloat-equal"
+    return a == b;
+#pragma GCC diagnostic pop
+}
+
+template <std::floating_point T>
 constexpr T negative_inf = -std::numeric_limits<T>::infinity();
 
 template <std::floating_point T>
