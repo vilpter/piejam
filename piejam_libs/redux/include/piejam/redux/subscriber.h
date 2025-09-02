dfc9cc65f4884352fbcc027434a8edccfcac33a6
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2025-09-02 20:39:28 +0200

    redux: update last subscriber value only when it changed

--- piejam_libs/redux/include/piejam/redux/subscriber.h
+++ piejam_libs/redux/include/piejam/redux/subscriber.h
@@ -57,9 +57,8 @@ public:
                     if (*last != current)
                     {
                         handler(current);
+                        *last = std::move(current);
                     }
-
-                    *last = current;
                 }),
                 token);
     }
