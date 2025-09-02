107cfeca57a8f4e6f0660f2d6d28c059b9f2f1a7
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2025-09-02 19:34:53 +0200

    audio: hide pitch_yin implementation
    also precompute the masks selection

--- piejam_libs/audio/include/piejam/audio/dsp/pitch_yin.h
+++ piejam_libs/audio/include/piejam/audio/dsp/pitch_yin.h
@@ -6,110 +6,14 @@
 
 #include <piejam/audio/sample_rate.h>
 
-#include <piejam/math/pow_n.h>
-#include <piejam/numeric/mipp_iterator.h>
-
-#include <mipp.h>
-
-#include <boost/hof/compose.hpp>
-
 #include <concepts>
-#include <numeric>
 #include <span>
 
 namespace piejam::audio::dsp
 {
 
-namespace detail
-{
-
 template <std::floating_point T>
 [[nodiscard]]
-static constexpr auto
-sqr_difference_sum(
-        std::span<T const> const in,
-        std::size_t const e,
-        std::size_t const tau) -> T
-{
-    constexpr auto N = mipp::N<T>();
-    auto const data = in.data();
-    auto const offset = tau % N;
-    if (offset == 0)
-    {
-        return mipp::sum(
-                std::transform_reduce(
-                        numeric::mipp_iterator{data},
-                        numeric::mipp_iterator{data + e},
-                        numeric::mipp_iterator{data + tau},
-                        mipp::Reg<T>(T{}),
-                        std::plus<>{},
-                        boost::hof::compose(math::pow_n<2>, std::minus<>{})));
-    }
-    else
-    {
-        mipp::Reg<T> sums(T{});
-
-        auto mask = []<std::size_t... I>(
-                            std::index_sequence<I...>,
-                            std::size_t offset) {
-            return mipp::Msk<N>{I < (N - offset)...};
-        }(std::make_index_sequence<N>{}, offset);
-
-        numeric::mipp_iterator it_tau{data + tau - offset};
-        mipp::Reg<T> reg_lo = numeric::mipp_lrot_n(*it_tau, offset);
-
-        for (auto reg_i : std::ranges::subrange(
-                     numeric::mipp_iterator{data},
-                     numeric::mipp_iterator{data + e}))
-        {
-            ++it_tau;
-            mipp::Reg<T> reg_hi = numeric::mipp_lrot_n(*it_tau, offset);
-
-            sums = numeric::mipp_fsqradd(
-                    reg_i - mipp::select(mask, reg_lo, reg_hi),
-                    sums);
-
-            reg_lo = reg_hi;
-        }
-
-        return mipp::sum(sums);
-    }
-}
-
-} // namespace detail
-
-template <std::floating_point T>
-[[nodiscard]]
-auto
-pitch_yin(std::span<T const> const in, sample_rate const sr) -> T
-{
-    constexpr T threshold{0.1f};
-
-    std::size_t const e = in.size() / 2;
-    T a = T{1};
-    T b = T{1};
-    T c{};
-    T cumulative_sum{};
-    for (std::size_t tau = 1; tau < e; ++tau)
-    {
-        T const sum = detail::sqr_difference_sum(in, e, tau);
-
-        cumulative_sum += sum;
-        c = tau * sum / cumulative_sum;
-
-        if (b < threshold && b < c)
-        {
-            // quadratic interpolation
-            T const betterTau =
-                    (tau - 1) + (a - c) / (T{2} * (a - T{2} * b + c));
-            return sr.as_float<T>() / betterTau;
-        }
-
-        a = b;
-        b = c;
-    }
-
-    return T{};
-}
+auto pitch_yin(std::span<T const> in, sample_rate) -> T;
 
 } // namespace piejam::audio::dsp
---
+++ piejam_libs/audio/src/piejam/audio/dsp/pitch_yin.cpp
@@ -0,0 +1,121 @@
+// PieJam - An audio mixer for Raspberry Pi.
+// SPDX-FileCopyrightText: 2020-2025  Dimitrij Kotrev
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <piejam/audio/dsp/pitch_yin.h>
+
+#include <piejam/math/pow_n.h>
+#include <piejam/numeric/mipp_iterator.h>
+
+#include <mipp.h>
+
+#include <boost/hof/compose.hpp>
+
+#include <array>
+#include <numeric>
+#include <utility>
+
+namespace piejam::audio::dsp
+{
+
+namespace
+{
+
+template <std::size_t N>
+std::array<mipp::Msk<N>, N> precomputed_masks =
+        []<std::size_t... Offset>(std::index_sequence<Offset...>) {
+            return std::array{[]<std::size_t... I>(
+                                      std::index_sequence<I...>,
+                                      std::size_t offset) {
+                return mipp::Msk<N>{I < (N - offset)...};
+            }(std::make_index_sequence<N>{}, Offset)...};
+        }(std::make_index_sequence<N>{});
+
+template <std::floating_point T>
+[[nodiscard]]
+constexpr auto
+sqr_difference_sum(
+        std::span<T const> const in,
+        std::size_t const e,
+        std::size_t const tau) -> T
+{
+    constexpr auto N = mipp::N<T>();
+    auto const data = in.data();
+    auto const offset = tau % N;
+    if (offset == 0)
+    {
+        return mipp::sum(
+                std::transform_reduce(
+                        numeric::mipp_iterator{data},
+                        numeric::mipp_iterator{data + e},
+                        numeric::mipp_iterator{data + tau},
+                        mipp::Reg<T>(T{}),
+                        std::plus<>{},
+                        boost::hof::compose(math::pow_n<2>, std::minus<>{})));
+    }
+    else
+    {
+        mipp::Reg<T> sums(T{});
+
+        auto mask = precomputed_masks<N>[offset];
+
+        numeric::mipp_iterator it_tau{data + tau - offset};
+        mipp::Reg<T> reg_lo = numeric::mipp_lrot_n(*it_tau, offset);
+
+        for (auto reg_i : std::ranges::subrange(
+                     numeric::mipp_iterator{data},
+                     numeric::mipp_iterator{data + e}))
+        {
+            ++it_tau;
+            mipp::Reg<T> reg_hi = numeric::mipp_lrot_n(*it_tau, offset);
+
+            sums = numeric::mipp_fsqradd(
+                    reg_i - mipp::select(mask, reg_lo, reg_hi),
+                    sums);
+
+            reg_lo = reg_hi;
+        }
+
+        return mipp::sum(sums);
+    }
+}
+
+} // namespace
+
+template <std::floating_point T>
+[[nodiscard]]
+auto
+pitch_yin(std::span<T const> const in, sample_rate const sr) -> T
+{
+    constexpr T threshold{0.1};
+
+    std::size_t const e = in.size() / 2;
+    T prev_cmn = T{1};
+    T curr_cmn = T{1};
+    T cumulative_sum{};
+    for (std::size_t tau = 1; tau < e; ++tau)
+    {
+        T const sum = sqr_difference_sum(in, e, tau);
+
+        cumulative_sum += sum;
+        T const next_cmn = tau * sum / cumulative_sum;
+
+        if (curr_cmn < threshold && curr_cmn < next_cmn)
+        {
+            // quadratic interpolation
+            T const denom = T{2} * (prev_cmn - T{2} * curr_cmn + next_cmn);
+            BOOST_ASSERT(std::abs(denom) > std::numeric_limits<T>::epsilon());
+            T const betterTau = (tau - 1) + (prev_cmn - next_cmn) / denom;
+            return sr.as_float<T>() / betterTau;
+        }
+
+        prev_cmn = curr_cmn;
+        curr_cmn = next_cmn;
+    }
+
+    return T{};
+}
+
+template auto pitch_yin<float>(std::span<float const>, sample_rate) -> float;
+
+} // namespace piejam::audio::dsp
