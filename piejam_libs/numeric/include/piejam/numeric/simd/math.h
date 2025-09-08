3cfb2b2f3a3c5988924f9d66f22788d4b647c3f3
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2025-09-08 19:20:10 +0200

    numeric: add lifted simd math operations

---
+++ piejam_libs/numeric/include/piejam/numeric/simd/math.h
@@ -0,0 +1,17 @@
+// PieJam - An audio mixer for Raspberry Pi.
+// SPDX-FileCopyrightText: 2020-2025  Dimitrij Kotrev
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <mipp.h>
+
+#include <boost/hof/lift.hpp>
+
+namespace piejam::numeric::simd
+{
+
+inline constexpr auto abs = BOOST_HOF_LIFT(mipp::abs);
+inline constexpr auto max = BOOST_HOF_LIFT(mipp::max);
+
+} // namespace piejam::numeric::simd
