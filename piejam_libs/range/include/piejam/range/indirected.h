e2ce904f4874c4f3726323a0f101243ea5ad05dc
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2025-10-05 19:32:08 +0200

    range: add simple indirected adaptor based on std::views::transform

---
+++ piejam_libs/range/include/piejam/range/indirected.h
@@ -0,0 +1,17 @@
+// PieJam - An audio mixer for Raspberry Pi.
+// SPDX-FileCopyrightText: 2020-2025  Dimitrij Kotrev
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <ranges>
+
+namespace piejam::range
+{
+
+inline constexpr auto indirected =
+        std::views::transform([](auto&& x) -> decltype(auto) {
+            return *std::forward<decltype(x)>(x);
+        });
+
+} // namespace piejam::range
