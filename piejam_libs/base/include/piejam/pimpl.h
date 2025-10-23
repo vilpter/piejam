7c309a70542ee9a4f183dbf6c9438d808ea62cf3
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2025-10-23 17:10:58 +0200

    base: add type erased pimpl deleter
    A type erased pimpl deleter allows assignment of derived
    pimpl pointers to a base pimpl pointer.

--- piejam_libs/base/include/piejam/pimpl.h
+++ piejam_libs/base/include/piejam/pimpl.h
@@ -4,19 +4,52 @@
 
 #pragma once
 
+#include <boost/assert.hpp>
+
 #include <memory>
 
 namespace piejam
 {
 
+namespace detail
+{
+
+struct pimpl_deleter
+{
+    constexpr pimpl_deleter() noexcept
+        : m_delete{[]([[maybe_unused]] void* p) { BOOST_ASSERT(!p); }}
+    {
+    }
+
+    template <class T>
+    constexpr pimpl_deleter(std::in_place_type_t<T>) noexcept
+        : m_delete{[](void* p) { delete static_cast<T*>(p); }}
+    {
+    }
+
+    template <class T>
+    void operator()(T* p) const noexcept
+    {
+        m_delete(p);
+    }
+
+private:
+    using delete_f = void (*)(void*);
+    delete_f m_delete;
+};
+
+} // namespace detail
+
 template <class T>
-using pimpl = std::unique_ptr<T, void (*)(T*)>;
+using pimpl = std::unique_ptr<T, detail::pimpl_deleter>;
 
 template <class T, class... Args>
 auto
 make_pimpl(Args&&... args) -> pimpl<T>
 {
-    return pimpl<T>{new T{std::forward<Args>(args)...}, [](T* p) { delete p; }};
+    return pimpl<T>{
+        new T{std::forward<Args>(args)...},
+        detail::pimpl_deleter{std::in_place_type<T>}};
 }
 
 } // namespace piejam
